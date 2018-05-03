0ef656bd4a33683d13cb8585b21720ce389205bf
YankiMuslu <yamuslu@gmail.com>
2018-05-03 10:40:10 +0200

    Drone v0.8

--- src/drone.py
+++ src/drone.py
@@ -1,4 +1,5 @@
-from interfaces import CommandObserver, RingObserver
+import time
+from interfaces import CommandObserver, RingObserver, Priority
 from modules.analyzer import Analyzer
 from modules.pathfinder import Pathfinder
 from modules.communication import Communication
@@ -6,48 +7,86 @@ from modules.communication import Communication
 
 
 class Drone(CommandObserver, RingObserver):
+
+    _penetrating = False
+
     def __init__(self):
-        self._number_of_rings = 10
+        self._analyzer = Analyzer()
+        self._communication = Communication()
+        self._pathfinder = Pathfinder()
+
+        self._video_URL = "tcp://localhost:5555"
+        self._number_of_rings = 10  # TODO: Real number? Determine dynamically? How long should it search?
         self._current_ring = 0
         self._rings = [None] * self._number_of_rings
-        self._analyzer = Analyzer(self)
-        self._communication = Communication()
-        self._pathfinder = Pathfinder(self)
+        self._commands = {Priority.Exploring: None, Priority.Approaching: None, Priority.Analyzing: None,
+                          Priority.Penetrating: None}
 
     def run(self):
-        """
-        PSEUDO CODE:
+        ready, msg = self._communication.test()
+        if ready:
+            # Initiate analyzer
+            self._analyzer.add_command_observer(self)
+            self._analyzer.add_command_observer(self)
+            self._analyzer.analyze_video(self._video_URL, self._rings)
+            self._analyzer.start()
+            # Initiate pathfinder
+            self._pathfinder.add_command_observer(self)
+            self._pathfinder.explore()
+            self._pathfinder.start()
+        else:
+            print(msg)
 
-        Test communications
-        initiate analyzer
-        initiate pathfinder
-        START EXPLORE
-        """
-        pass
-
-    def initiate_drone_configuration(self):
+    def _initiate_drone_configuration(self):
+        # TODO: Implement
         pass
 
     def receive_command(self, command, priority):
-        # TODO: Command is received from analyzer of pathfinder, determine how to react based on priority
+        self._commands[priority] = command
+
+    def _send_command(self):
+        while True:
+            time.sleep(0.03)
+            if self._penetrating and self._commands[Priority.Penetrating] is None:
+                continue
+            else:
+                command = next((el for el in self._commands if el is not None), None)
+            if command is not None:
+                self._communication.move(command)
         pass
 
     def add_ring(self, ring):
-        # TODO: Ring is found by analyzer (probably while exploring), add it to the list of rings
-        """
-        PSEUDO CODE:
+        self._rings[ring.get_qr_number()] = ring
+        if ring.get_qr_number() is self._current_ring:
+            self._pathfinder.pause()
+            self._pathfinder.penetrate_ring(ring, self.update_state())
 
-        rings[ring.qr] = ring
-        if ring.qr == _current_ring:
-            PAUSE EXPLORE
-            PENETRATE
+    def update_state(self):
+        if self._current_ring == self._number_of_rings:
+            # Final ring penetrated - exit the game
+            self._pathfinder.stop()
+            self._analyzer.stop()
+            self._communication.land()
+        else:
+            # More rings to penetrate - continue exploring
+            self._pathfinder.start()
 
-            when PENETRATE is done:
-                if ring.qr == _number_of_rings:
-                    STOP EXPLORE
-                    STOP ANALYZER
-                    LAND DRONE
-                else:
-                    START EXPLORE
-        """
-        pass
+    def add_command_observer(self, observer):
+        self._pathfinder.add_command_observer(observer)
+        self._analyzer.add_command_observer(observer)
+
+    def add_ring_observer(self, observer):
+        self._analyzer.add_ring_observer(observer)
+
+    def add_analyzed_video_observer(self, observer):
+        self._analyzer.add_analyzed_video_observer(observer)
+
+    def del_command_observer(self, observer):
+        self._pathfinder.del_command_observer(observer)
+        self._analyzer.del_command_observer(observer)
+
+    def del_ring_observer(self, observer):
+        self._analyzer.del_ring_observer(observer)
+
+    def del_analyzed_video_observer(self, observer):
+        self._analyzer.del_analyzed_video_observer(observer)
